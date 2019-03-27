0f35f1eaac759dcdee78e243cafbab91172f898e
Bissane <Bissane@Bissane.local>
2019-03-27 13:55:25 +0100

    thirdUpdate

--- boards/models.py
+++ boards/models.py
@@ -25,8 +25,8 @@ class Board(models.Model):
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, related_name='topics')
-    starter = models.ForeignKey(User, related_name='topics')
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
+    starter = models.ForeignKey(User, on_delete=models.CASCADE,related_name='topics')
     views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
@@ -54,11 +54,11 @@ class Topic(models.Model):
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(Topic, related_name='posts')
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE,related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, related_name='posts')
-    updated_by = models.ForeignKey(User, null=True, related_name='+')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,related_name='posts')
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE,null=True, related_name='+')
 
     def __str__(self):
         truncated_message = Truncator(self.message)
